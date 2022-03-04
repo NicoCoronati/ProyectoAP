42b8cdaba6b92f47f96d36c26d3f52d5d28c511e
NicoCoronati <NicoCoronati@users.noreply.github.com>
2022-03-04 17:55:31 -0300

    retomo el portfolio
    retomando portfolio, haciendo un maquetado front

--- Portfolio/portfolio/src/app/app.module.ts
+++ Portfolio/portfolio/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { ButtonComponent } from './componentes/button/button.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    AboutComponent,
+    ExperienciaComponent,
   ],
   imports: [
     BrowserModule
