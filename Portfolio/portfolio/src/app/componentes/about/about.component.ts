1adbaee3454d401058723a93cca3cb471d2b9a69
NicoCoronati <NicoCoronati@users.noreply.github.com>
2022-04-28 17:17:23 -0300

    Volviendo al proyecto
    Intentando el responsive

--- Portfolio/portfolio/src/app/componentes/about/about.component.ts
+++ Portfolio/portfolio/src/app/componentes/about/about.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  textacercade:string = 'Soy estudiante de Ingeniería en Sistemas en la UTN FRC, tengo 21 años, me gusta programar desde los 11 años, momento que quise tener mi propio Argentum Online, practique atletismo a un nivel competitivo y me gusta mucho la música.       ?¿??¿';
+  
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  cambiar_parrafo(): void {
+    console.log("TEST")
+    document.getElementById("editacercade")!.style.display="block";
+    let texto = document.getElementById("textacercade")!.innerText;
+  }
+
+  myFunction2(valor:string){
+    console.log("TEST2")
+    
+    console.log(valor + "asd");
+    document.getElementById("textacercade")!.innerText = valor;
+
+  }
 }
