e8e1f62e84ce6ba63e8402dedf71f1981ecd0f4e
NicoCoronati <NicoCoronati@users.noreply.github.com>
2022-08-04 19:01:40 -0300

    Agregado el cors

--- Portfolio/portfolio/BackEnd/src/main/java/com/portfolioAP/Corosoft/Controller/ProyectoController.java
+++ Portfolio/portfolio/BackEnd/src/main/java/com/portfolioAP/Corosoft/Controller/ProyectoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ProyectoController {
     @Autowired 
     private IProyectoService IproyectoService;
