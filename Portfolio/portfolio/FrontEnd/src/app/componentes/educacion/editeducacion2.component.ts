0952550d147dcd28dff6ebfa1a7d42840157e23f
NicoCoronati <NicoCoronati@users.noreply.github.com>
2022-08-29 19:55:58 -0300

    testing

--- Portfolio/portfolio/FrontEnd/src/app/componentes/educacion/editeducacion2.component.ts
+++ Portfolio/portfolio/FrontEnd/src/app/componentes/educacion/editeducacion2.component.ts
@@ -33,7 +33,7 @@ export class Editeducacion2Component implements OnInit {
       data => {
         this.router.navigate(['']);
       }, err => {
-        alert("error");
+        alert("error2");
         this.router.navigate(['']);
       }
     )
